7b3cfa802301fe91b0b34152a1da7f65f2da86df
ballinlikeme <maxim.spirkin2003@gmail.com>
2023-03-17 23:46:44 +0300

    refactor: wrapper style

---
+++ src/modules/RegistrationForm/ui/StyledWrapper.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  
+  background-color: var(--assist-color);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
